fe3399411c9b66662ef469319692fdef55b8c587
Jose Guerra <jose@gcostudios.com>
2018-10-04 20:01:13 +0100

    added svg support

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpe?g)/i,
+        test: /\.(png|svg|jpe?g)/i,
         use: [
           {
             loader: "url-loader",
