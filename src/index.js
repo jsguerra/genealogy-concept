57de6f88c97fbaea74391c1b5f769407917c8c6f
Jose Guerra <jose.guerra@finalsite.com>
2018-12-06 09:23:48 +0000

    updated file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
+// require main js script
+//require('./js/script.js');
+
 // Javascript goes here
 import style from "./sass/main.scss";
 
-// Genealogy Template
-export function Person(name, birthYear,deathYear) {
-	this.name = name;
-	this.birthYear = birthYear;
-	this.deathYear = deathYear;
-}
+import Person from './js/script.js';
